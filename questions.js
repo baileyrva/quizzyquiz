22fed29a49cc605089078e1ead7f6f6bdba2ebb1
Joe Bailey <baileyrva@gmail.com>
2019-12-01 10:06:49 -0500

    Updated timer increase/decrease and began end page

--- questions.js
+++ questions.js
@@ -1,11 +1,13 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let time = 30;
 
 let questions = [
   {
@@ -61,33 +63,73 @@ function startGame() {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
-        return window.location.assign("/end.html"); 
-    }
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
   choices.forEach(choice => {
-    const number = choice.dataset['number'];
-    choice.innerText = currentQuestion['choice' + number];
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
   });
 
   availableQuestions.splice(questionIndex, 1);
 
   acceptingAnswers = true;
-}
+};
 
 choices.forEach(choice => {
   choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
-    getNewQuestion();
+    let selectedChoice = e.target;
+    let selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply = "incorrect";
+    if (selectedAnswer == currentQuestion.answer) {
+      classToApply = "correct";
+    }
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    if(classToApply === "incorrect") {
+        scorePenalty(); 
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
+
+decrementTime = () => {
+  time = time - 1;
+  if (time < 30) {
+    timer.innerHTML = time;
+  }
+  if (time < 1) {
+      return window.location.assign("/end.html");
+  }
+}
+update = setInterval("decrementTime()", 1000)
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+scorePenalty = () => {
+    time = time - 3; 
+}
 
 startGame();
